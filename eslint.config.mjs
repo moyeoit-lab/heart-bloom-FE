2077225352e8abd07f3bdeb8618b748b824faf07
yura0302 <yuraa0302@gmail.com>
2026-04-19 17:37:57 +0900

    chore: eslint flat config 전환 및 husky nvm 환경 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,26 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginNext from "@next/eslint-plugin-next";
-import storybook from "eslint-plugin-storybook";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextConfig from "eslint-config-next";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 
 const eslintConfig = [
-  ...compat.config({
-    plugins: ["@next/next", "@tanstack/query"],
-    extends: [
-      "next/core-web-vitals",
-      "next/typescript",
-      "prettier",
-      "plugin:@tanstack/query/recommended",
-      "plugin:storybook/recommended",
-    ],
+  ...nextConfig,
+  ...tanstackQuery.configs["flat/recommended"],
+  {
     rules: {
-      ...pluginNext.configs.recommended.rules,
       "@tanstack/query/exhaustive-deps": "error",
       "@tanstack/query/stable-query-client": "error",
       "@tanstack/query/no-rest-destructuring": "warn",
       "@tanstack/query/no-unstable-deps": "warn",
     },
-    ignorePatterns: [
+  },
+  {
+    ignores: [
       "**/node_modules/**",
       "**/public/**",
       "**/dist/**",
       ".next/**",
       "out/**",
-      "storybook-static/**",
-      ".pnpm-lock.yaml",
     ],
-  }),
-  ...storybook.configs["flat/recommended"],
+  },
 ];
 
 export default eslintConfig;
